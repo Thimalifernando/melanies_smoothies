332c1a19293dcfecd383fccb9cd6a3042a04c622
Thimalifernando <133652857+Thimalifernando@users.noreply.github.com>
2025-04-22 11:02:31 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -12,8 +12,6 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be:',name_on_order)
 
-
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
 
 ingredient_list = st.multiselect(
