3ab5125e4e6dfbb89d41b913ecf59bfa8a358ff0
Thimalifernando <133652857+Thimalifernando@users.noreply.github.com>
2025-04-22 13:33:18 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -19,8 +19,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit
 # st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredient_list = st.multiselect(
     'Choose upto 5 ingredients:',
@@ -33,9 +33,13 @@ if ingredient_list:
     ingredients_string =''
     for fruit_chosen in ingredient_list:
         ingredients_string+= fruit_chosen
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen , 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ',fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
-        sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
+        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+        sf_df = st.dataframe(data = fruityvice_response.json(),use_container_width = True)
 
     
     my_insert_stmt = f"""
